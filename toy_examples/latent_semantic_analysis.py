6e75e97e283838924e33b1eb0c9388e4b5f74ec7
Sile <billtaosile@hotmail.com>
2022-11-09 00:01:32 -0500

    add svd word vec

---
+++ toy_examples/latent_semantic_analysis.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# corpus = [
+#     'I like deep learning.',
+#     'I like NLP.',
+#     'I enjoy flying.',
+# ]
+
+def plot_word_vectors(U, words):
+    fontsize = 15
+    for i in range(len(words)):
+        plt.text(U[i, 0], U[i, 1], words[i], fontsize=fontsize)
+    plt.xlim([-1, 0.5])
+    plt.ylim([-1, 1])
+    plt.show()
+
+
+words = ["I", "like", "enjoy", "deep", "learning", "NLP", "flying", "."]
+cooc_arr = np.array([[0, 2, 1, 0, 0, 0, 0, 0],
+                     [2, 0, 0, 1, 0, 1, 0, 0],
+                     [1, 0, 0, 0, 0, 0, 1, 0],
+                     [0, 1, 0, 0, 1, 0, 0, 0],
+                     [0, 0, 0, 1, 0, 0, 0, 1],
+                     [0, 1, 0, 0, 0, 0, 0, 1],
+                     [0, 0, 1, 0, 0, 0, 0, 1],
+                     [0, 0, 0, 0, 1, 1, 1, 0],
+                     ])
+U, s, Vh = np.linalg.svd(cooc_arr, full_matrices=False)
+print(f'U.shape, s.shape, Vh.shape: {U.shape, s.shape, Vh.shape}')
+
+# For a real symmetric matrix,
+#   (1) SVD is equivalent to eigen-decompostion;
+#   (2) U and Vh.T are equivalent up to sign changes
+print(f'U:\n {np.round(U, 2)}\n')
+print(f'Vh.T:\n {np.round(Vh.T, 2)}')
+
+plot_word_vectors(U, words)
