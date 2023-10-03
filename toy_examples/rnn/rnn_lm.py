23b047806ef3f79681780a0aa734ae5b957f8b67
Sile <billtaosile@hotmail.com>
2023-10-02 23:42:40 -0400

    refactor

--- toy_examples/rnn/rnn_lm.py
+++ toy_examples/rnn/rnn_lm.py
@@ -1,11 +1,13 @@
+import matplotlib.pyplot as plt
 import torch
+from d2l import torch as d2l
 from torch import nn
 from torch.nn import functional as F
 
 from rnn_scratch import RNNScratch
 
 
-class RNNLMScratch(nn.Module):
+class RNNLMScratch(d2l.Classifier):
     """The RNN-based language model implemented from scratch."""
 
     def __init__(self, rnn, vocab_size, lr=0.01):
@@ -13,8 +15,10 @@ class RNNLMScratch(nn.Module):
         self.rnn = rnn
         self.vocab_size = vocab_size
         self.init_params()
+        self.lr = lr
 
     def init_params(self):
+        # weights in the output layer
         self.W_hq = nn.Parameter(
             torch.randn(self.rnn.num_hiddens, self.vocab_size) * self.rnn.sigma)
         self.b_q = nn.Parameter(torch.zeros(self.vocab_size))
@@ -40,19 +44,29 @@ class RNNLMScratch(nn.Module):
         print(f'X: {X}')
         embs = self.one_hot(X)
         print(f'embs: {embs}')
-        quit()
+        print(f'embs.shape: {embs.shape}')  # embs.shape: torch.Size([32, 2, 28])
 
         rnn_outputs, _ = self.rnn(embs, state)
         return self.output_layer(rnn_outputs)
 
 
 if __name__ == "__main__":
-    batch_size, num_inputs, num_hiddens, num_steps = 2, 4, 3, 5
-    rnn = RNNScratch(num_inputs, num_hiddens)
+    # batch_size, num_inputs, num_hiddens, num_steps = 2, 4, 3, 5
+    # rnn = RNNScratch(num_inputs, num_hiddens)
+    #
+    # model = RNNLMScratch(rnn, vocab_size=num_inputs)
+    # outputs = model(torch.ones((batch_size, num_steps), dtype=torch.int64))
+    # print('\n\n')
+    # print(f'batch_size, num_inputs, num_hiddens, num_steps: {batch_size, num_inputs, num_hiddens, num_steps}')
+    # print(f'outputs: {outputs}')
+    # print(f'outputs.shape: {outputs.shape}')
 
-    model = RNNLMScratch(rnn, vocab_size=num_inputs)
-    outputs = model(torch.ones((batch_size, num_steps), dtype=torch.int64))
-    print('\n\n')
-    print(f'batch_size, num_inputs, num_hiddens, num_steps: {batch_size, num_inputs, num_hiddens, num_steps}')
-    print(f'outputs: {outputs}')
-    print(f'outputs.shape: {outputs.shape}')
+    data = d2l.TimeMachine(batch_size=1024, num_steps=32)
+    print(f'data.vocab.token_to_idx: {data.vocab.token_to_idx}')
+    # quit()
+
+    rnn = RNNScratch(num_inputs=len(data.vocab), num_hiddens=32)
+    model = RNNLMScratch(rnn, vocab_size=len(data.vocab), lr=1)
+    trainer = d2l.Trainer(max_epochs=5, gradient_clip_val=1, num_gpus=1)
+    trainer.fit(model, data)
+    plt.show()
